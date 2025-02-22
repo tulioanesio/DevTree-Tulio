4997860d46b8a3aba9c79409d90ab5a7ca81fb7e
Tulio <tulioluv@gmail.com>
2025-02-22 13:54:37 -0300

    feat: responsividade com celulares e tablets.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle("light")
+  const html = document.documentElement;
+  html.classList.toggle("light");
 }
